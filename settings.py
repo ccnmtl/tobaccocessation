cce45303675dfbc0facbdbd3d427e1645f423b7d
sdreher <sdreher@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-09-20 14:42:54 +0000

    * integrating pagetree & pageblocks code from anders. * partitioning off code that i think can be merged into pagetree/pageblocks into siteeditor * tweaking the templates & styles to allow me to use the 2 columns for the editor area and the base for the main page views.
    git-svn-id: http://svn.ccnmtl.columbia.edu/tobaccocessation/trunk@16772 1f418930-7ff8-0310-b8ad-c653122473bc

--- settings.py
+++ settings.py
@@ -2,3 +2,4 @@ from settings_shared import *
 
 DATABASE_USER='postgres'
 DATABASE_PASSWORD='postgres'
+MEDIA_ROOT = "/Users/sdreher/Documents/workspace/tobaccocessation/uploads/"
--- urls.py
+++ urls.py
@@ -7,18 +7,27 @@ admin.autodiscover()
 site_media_root = os.path.join(os.path.dirname(__file__),"media")
 
 urlpatterns = patterns('',
-                       # Example:
-                       # (r'^tobaccocessation/', include('tobaccocessation.foo.urls')),
-                       ('^$','django.views.generic.simple.redirect_to', {'url':'/welcome/'}),
+                       (r'^$','siteeditor.views.index'),
                        (r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'logged_out.html'}),
-                       (r'^resources/', include('tobaccocessation.resources.urls')),
-                       (r'^welcome/', include('tobaccocessation.welcome.urls')),
-                       (r'^assist/', include('tobaccocessation.assist.urls')),
+                       #(r'^resources/', include('tobaccocessation.resources.urls')),
+                       #(r'^welcome/', include('tobaccocessation.welcome.urls')),
+                       #(r'^assist/', include('tobaccocessation.assist.urls')),
                        ('^accounts/',include('djangowind.urls')),
                        (r'^admin/(.*)', admin.site.root),
 		               (r'^survey/',include('survey.urls')),
                        (r'^tinymce/', include('tinymce.urls')),
                        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': site_media_root}),
                        (r'^uploads/(?P<path>.*)$','django.views.static.serve',{'document_root' : settings.MEDIA_ROOT}),
-        
+                       (r'^pagetree/',include('pagetree.urls')),
+                       
+                       (r'^edit/(?P<path>.*)add_child_section/$','siteeditor.views.add_child_section'),
+                       (r'^edit/(?P<path>.*)add_textblock/$','siteeditor.views.add_textblock'),
+                       (r'^edit/(?P<path>.*)add_imageblock/$','siteeditor.views.add_imageblock'),
+                       (r'^edit/(?P<path>.*)add_imagepullquoteblock/$','siteeditor.views.add_imagepullquoteblock'),
+                       (r'^edit/(?P<path>.*)add_htmlblock/$','siteeditor.views.add_htmlblock'),
+                       (r'^edit/(?P<path>.*)add_pullquoteblock/$','siteeditor.views.add_pullquoteblock'),
+                       
+                       # very important that these two stay last and in this order
+                       (r'^edit/(?P<path>.*)$','siteeditor.views.edit_page'),
+                       (r'^(?P<path>.*)$','siteeditor.views.page'),        
 )
