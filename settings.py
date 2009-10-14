8a3dc4f180a6f54d291ca339f1d39416559b5425
lizday <lizday@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-10-14 21:13:25 +0000

    begin to skin
    git-svn-id: http://svn.ccnmtl.columbia.edu/tobaccocessation/trunk@17451 1f418930-7ff8-0310-b8ad-c653122473bc

--- settings.py
+++ settings.py
@@ -3,3 +3,4 @@ from settings_shared import *
 DATABASE_USER='postgres'
 DATABASE_PASSWORD='postgres'
 MEDIA_ROOT = "/Users/sdreher/Documents/workspace/tobaccocessation/uploads/"
+DATABASE_NAME='tobacco_liz'
