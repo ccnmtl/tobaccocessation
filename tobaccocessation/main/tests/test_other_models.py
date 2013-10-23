681329703785ec2f59ed2a4107c5a4c1fd74c437
Cassandra Dunlop <cdunlop@kodos.ccnmtl.columbia.edu>
2013-10-23 12:09:25 -0400

    some more tests

---
+++ tobaccocessation/main/tests/test_other_models.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+from django.test.client import Client
+from tobaccocessation.activity_treatment_choice.models import Block
+
+class TestModelsNoUser(TestCase):
+    def setUp(self):
+        self.block = Block()
+        self.block.save()
+
+    def test_block_submit_false(self):
+        self.assertEquals(self.block.needs_submit(), False)
+
+    def test_block_edit_form(self):
+        self.form_test = self.block.edit_form()
+        self.assertIsNotNone(self.form_test)
+
+    def test_block_add_form(self):
+        self.add_form_test = self.block.add_form()
+        self.assertIsNotNone(self.add_form_test)
+
+
+    # def test_block_create(self):
+    #     c = Client()
+    #     self.request = c.post('/some_page/', {'medication_name': 'medication name', 'show_correct': False})
+    #     self.block_create = self.block.create(self.request)
+    #     self.assertIsNotNone(self.block_create)
