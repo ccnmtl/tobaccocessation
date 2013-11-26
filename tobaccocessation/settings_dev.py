0c53ae1d17019dba38af8590881d47a2a4162c6f
Susan Dreher <sdreher@columbia.edu>
2013-11-26 14:06:09 -0500

    kodos (qa) configuration files

---
+++ tobaccocessation/settings_dev.py
@@ -0,0 +1,50 @@
+# flake8: noqa
+from tobaccocessation.settings import *
+
+TEMPLATE_DIRS = (
+    "/var/www/tobaccocessation/tobaccocessation/tobaccocessation/templates",
+)
+
+MEDIA_ROOT = '/var/www/tobaccocessation/uploads/'
+# put any static media here to override app served static media
+STATICMEDIA_MOUNTS = (
+    ('/sitemedia', '/var/www/tobaccocessation/tobaccocessation/sitemedia'),
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tobaccocessation',
+        'HOST': '',
+        'PORT': '',
+        'USER': '',
+        'PASSWORD': '',
+    }
+}
+
+COMPRESS_ROOT = "/var/www/tobaccocessation/tobaccocessation/media/"
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+DEV_ENV = True
+
+STATSD_PREFIX = 'tobaccocessation-dev'
+SENTRY_SITE = 'tobaccocessation-dev'
+SENTRY_SERVERS = ['http://sentry.ccnmtl.columbia.edu/sentry/store/']
+
+if 'migrate' not in sys.argv:
+    INSTALLED_APPS.append('raven.contrib.django')
+
+    import logging
+    from raven.contrib.django.handlers import SentryHandler
+    logger = logging.getLogger()
+    # ensure we havent already registered the handler
+    if SentryHandler not in map(type, logger.handlers):
+        logger.addHandler(SentryHandler())
+        logger = logging.getLogger('sentry.errors')
+        logger.propagate = False
+        logger.addHandler(logging.StreamHandler())
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
