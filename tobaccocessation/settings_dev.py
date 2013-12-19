adb04318b1f9738c2d4690a99960d4ccbc888f43
Susan Dreher <sdreher@columbia.edu>
2013-12-19 10:34:08 -0500

    settings_dev incorrect for kodos.

--- tobaccocessation/settings_dev.py
+++ tobaccocessation/settings_dev.py
@@ -1,16 +1,6 @@
 # flake8: noqa
 from tobaccocessation.settings import *
 
-TEMPLATE_DIRS = (
-    "/var/www/tobaccocessation/tobaccocessation/tobaccocessation/templates",
-)
-
-MEDIA_ROOT = '/var/www/tobaccocessation/uploads/'
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', '/var/www/tobaccocessation/tobaccocessation/sitemedia'),
-)
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -21,11 +11,6 @@ DATABASES = {
         'PASSWORD': '',
     }
 }
-
-COMPRESS_ROOT = "/var/www/tobaccocessation/tobaccocessation/media/"
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-DEV_ENV = True
 
 try:
     from local_settings import *
