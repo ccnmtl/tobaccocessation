9f64e57bf399e3d511523d19d1f321c4c4aa387c
Susan Dreher <sdreher@columbia.edu>
2014-02-11 14:17:11 -0500

    * switch to new raven * moving the activities navbar item under the resources page * adding a home icon * major css cleanup * some very basic styling for registration

--- tobaccocessation/activity_virtual_patient/tests/test_models.py
+++ tobaccocessation/activity_virtual_patient/tests/test_models.py
@@ -6,58 +6,61 @@ from tobaccocessation.activity_virtual_patient.models import \
 
 class TestMedication(TestCase):
     def test_unicode(self):
-        m = Medication.objects.create(
+        med = Medication.objects.create(
             name="foo",
             instructions="instructions",
             display_order=1,
             tag="none")
-        self.assertEqual(str(m), "foo")
+        self.assertEqual(str(med), "foo")
 
 
 class TestPatient(TestCase):
     fixtures = ['virtualpatient.json']
 
     def test_unicode(self):
-        p = Patient.objects.get(display_order=1)
-        self.assertEqual(str(p), "1. Beverly Johnson")
+        patient = Patient.objects.get(display_order=1)
+        self.assertEqual(str(patient), "1. Beverly Johnson")
 
     def test_treatments(self):
-        p = Patient.objects.get(display_order=1)
-        treatments = p.treatments()
+        patient = Patient.objects.get(display_order=1)
+        treatments = patient.treatments()
         self.assertEquals(len(treatments), 7)
 
-        p = Patient.objects.get(display_order=2)
-        treatments = p.treatments()
+        patient = Patient.objects.get(display_order=2)
+        treatments = patient.treatments()
         self.assertEquals(len(treatments), 8)
 
 
 class TestTreatmentClassification(TestCase):
     def test_unicode(self):
-        tc = TreatmentClassification.objects.create(rank=1, description="foo")
-        self.assertEqual(str(tc), "1. foo")
+        choice = TreatmentClassification.objects.create(rank=1,
+                                                        description="foo")
+        self.assertEqual(str(choice), "1. foo")
 
 
 class TestTreatmentOption(TestCase):
     def test_unicode(self):
-        p = Patient.objects.create(
+        patient = Patient.objects.create(
             name="foo",
             description="bar",
             history="history",
             display_order=1)
-        m1 = Medication.objects.create(
-            name="m1",
+        med1 = Medication.objects.create(
+            name="med1",
             instructions="instructions",
             display_order=1,
             tag="none")
-        m2 = Medication.objects.create(
-            name="m2",
+        med2 = Medication.objects.create(
+            name="med2",
             instructions="instructions",
             display_order=2,
             tag="none")
-        tc = TreatmentClassification.objects.create(rank=1, description="foo")
-        to = TreatmentOption.objects.create(
-            patient=p, classification=tc, medication_one=m1, medication_two=m2)
-        self.assertEqual(str(to), "Option: foo [m1, m2]")
+        choice = TreatmentClassification.objects.create(rank=1,
+                                                        description="foo")
+        option = TreatmentOption.objects.create(
+            patient=patient, classification=choice,
+            medication_one=med1, medication_two=med2)
+        self.assertEqual(str(option), "Option: foo [med1, med2]")
 
 
 class TestTreatmentOptionReasoning(TestCase):
