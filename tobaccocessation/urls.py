912f57c9b31374587b27192ac6467fe9c36634f4
Susan Dreher <sdreher@columbia.edu>
2014-05-22 13:29:19 -0400

    * upgrade djangowind version to pickup the latest Django 1.6 change password & registration urls * properly order the djangowind or auth_urls above django-registration module url inclusion

--- tobaccocessation/urls.py
+++ tobaccocessation/urls.py
@@ -2,52 +2,39 @@ from django.conf import settings
 from django.conf.urls import include, patterns, url
 from django.contrib import admin
 from django.views.generic import TemplateView
-import os.path
-admin.autodiscover()
 from registration.backends.default.views import RegistrationView
 from tobaccocessation.main.models import CreateAccountForm
-
+import os.path
+admin.autodiscover()
 
 site_media_root = os.path.join(os.path.dirname(__file__), "../media")
 
-login_page = (r'^accounts/', include('django.contrib.auth.urls'))
+auth_urls = (r'^accounts/', include('django.contrib.auth.urls'))
 if hasattr(settings, 'WIND_BASE'):
-    login_page = (r'^accounts/', include('djangowind.urls'))
     auth_urls = (r'^accounts/', include('djangowind.urls'))
-    logout_page = (
-        r'^accounts/logout/$',
-        'djangowind.views.logout',
-        {'next_page': '/'})
-redirect_after_logout = getattr(settings, 'LOGOUT_REDIRECT_URL', None)
 
 urlpatterns = patterns(
-    '',  # 'django.views.generic.simple',
+    '',
+    (r'^crossdomain.xml$',
+     'django.views.static.serve',
+     {'document_root': os.path.abspath(os.path.dirname(__file__)),
+      'path': 'crossdomain.xml'}),
     (r'^about/',
      TemplateView.as_view(template_name="flatpages/about.html")),
     (r'^help/',
      TemplateView.as_view(template_name="flatpages/help.html")),
     (r'^contact/',
      TemplateView.as_view(template_name="flatpages/contact.html")),
-)
+    url(r'^$', 'tobaccocessation.main.views.index', name="index"),
 
-urlpatterns += patterns(
-    '',
-    (r'^crossdomain.xml$',
-     'django.views.static.serve',
-     {'document_root': os.path.abspath(os.path.dirname(__file__)),
-      'path': 'crossdomain.xml'}),
-    url(r'^$',
-        'tobaccocessation.main.views.index',
-        name="index"),
-    (r'^accounts/logout/$',
-     'django.contrib.auth.views.logout',
-     {'next_page': redirect_after_logout}),
-    login_page,  # see above
+    auth_urls,
 
     url(r'^accounts/register/$', RegistrationView.as_view(
         form_class=CreateAccountForm),
         name='registration_register'),
+
     (r'^accounts/', include('registration.backends.default.urls')),
+
     (r'^admin/', include(admin.site.urls)),
     (r'^smoketest/', include('smoketest.urls')),
     (r'^main/', include('tobaccocessation.main.urls')),
