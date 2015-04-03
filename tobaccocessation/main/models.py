135195fdb3be670ab70f6b7a00b13f471d2d366f
Susan Dreher <sdreher@columbia.edu>
2015-04-02 21:27:57 -0400

    Swap out UserPageVisit for UserLocation. failing test with UserLocation

--- tobaccocessation/main/models.py
+++ tobaccocessation/main/models.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
-from pagetree.models import Hierarchy, UserLocation, UserPageVisit
+from pagetree.models import Hierarchy, UserPageVisit
 from quizblock.models import Submission, Response
 from registration.forms import RegistrationForm
 from registration.signals import user_registered
@@ -85,12 +85,13 @@ class UserProfile(models.Model):
 
     def last_location(self):
         hierarchy = Hierarchy.get_hierarchy(self.role())
-        try:
-            UserLocation.objects.get(user=self.user,
-                                     hierarchy=hierarchy)
-            return hierarchy.get_user_section(self.user)
-        except UserLocation.DoesNotExist:
+        visits = UserPageVisit.objects.filter(
+            user=self.user).order_by('-last_visit')
+
+        if visits.count() < 1:
             return hierarchy.get_first_leaf(hierarchy.get_root())
+        else:
+            return visits.first().section
 
     def percent_complete(self):
         hierarchy = Hierarchy.get_hierarchy(self.role())
