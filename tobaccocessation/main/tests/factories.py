df01efa85d7f9cf49e132da905024831495aeddf
Susan Dreher <sdreher@columbia.edu>
2015-12-07 16:10:19 -0500

    factories

---
+++ tobaccocessation/main/tests/factories.py
@@ -0,0 +1,19 @@
+import factory
+from pagetree.tests.factories import UserFactory
+
+from tobaccocessation.main.models import UserProfile
+
+
+class UserProfileFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = UserProfile
+
+    user = factory.SubFactory(UserFactory)
+    gender = 'M'
+    is_faculty = 'ST'
+    institute = 'I1'
+    specialty = 'S1'
+    hispanic_latino = 'Y'
+    year_of_graduation = 2015
+    consent_participant = True
+    consent_not_participant = False
