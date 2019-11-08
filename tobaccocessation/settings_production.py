3f66d54cbd665ef3ab129797bca41bc4fbe7cedc
Nikolas Nyby <nikolas@gnu.org>
2019-11-08 14:07:18 -0500

    migrate to sentry-sdk

--- tobaccocessation/settings_production.py
+++ tobaccocessation/settings_production.py
@@ -1,16 +1,25 @@
-# flake8: noqa
-from tobaccocessation.settings_shared import *
+from django.conf import settings
+from tobaccocessation.settings_shared import *  # noqa: F403
 from ccnmtlsettings.production import common
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 
 locals().update(
     common(
-        project=project,
-        base=base,
-        STATIC_ROOT=STATIC_ROOT,
-        INSTALLED_APPS=INSTALLED_APPS,
+        project=project,  # noqa: F405
+        base=base,  # noqa: F405
+        STATIC_ROOT=STATIC_ROOT,  # noqa: F405
+        INSTALLED_APPS=INSTALLED_APPS,  # noqa: F405
     ))
 
 try:
-    from tobaccocessation.local_settings import *
+    from tobaccocessation.local_settings import *  # noqa: F403
 except ImportError:
     pass
+
+if hasattr(settings, 'SENTRY_DSN'):
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,  # noqa: F405
+        integrations=[DjangoIntegration()],
+    )
